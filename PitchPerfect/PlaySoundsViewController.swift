6d2a1067c75e4042b84679e0484bace19596972e
Mustafa Muhammad Saad <eng.mustafa.muhammad@gmail.com>
2018-12-07 07:30:49 +0300

    Adding PlaySounds ViewController

---
+++ PitchPerfect/PlaySoundsViewController.swift
@@ -0,0 +1,32 @@
+//
+//  PlaySoundsViewController.swift
+//  PitchPerfect
+//
+//  Created by Mustafa Muhammad on 12/7/18.
+//  Copyright © 2018 Ara Tech. All rights reserved.
+//
+
+import UIKit
+
+class PlaySoundsViewController: UIViewController {
+
+    var recordAudioUrl:URL!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- PitchPerfect/RecordSoundsViewController.swift
+++ PitchPerfect/RecordSoundsViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import AVFoundation
 
 
-class RecordSoundsViewController: UIViewController {
+class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
 
     var state:RecordingState = .readyToRecord
     
@@ -38,11 +38,12 @@ class RecordSoundsViewController: UIViewController {
         let recordingName = "recordedVoice.wav"
         let pathArray = [dirPath, recordingName]
         let filePath = URL(string: pathArray.joined(separator: "/"))
-        print(filePath)
+        
         let session = AVAudioSession.sharedInstance()
         try! session.setCategory(AVAudioSession.Category.playAndRecord, mode: AVAudioSession.Mode.default, options: AVAudioSession.CategoryOptions.defaultToSpeaker)
         
         try! audioRecorder = AVAudioRecorder(url: filePath!, settings: [:])
+        audioRecorder.delegate = self
         audioRecorder.isMeteringEnabled = true
         audioRecorder.prepareToRecord()
         audioRecorder.record()
@@ -66,6 +67,15 @@ class RecordSoundsViewController: UIViewController {
         messageLabel.text = state == .readyToRecord ? "Tap to record" : "Recording, tap to stop"
         startButton.isEnabled = state == .readyToRecord
         stopButton.isEnabled = state == .recording
+    }
+    
+    
+    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
+        if(flag){
+            performSegue(withIdentifier: "stopRecording", sender: recorder.url)
+        }else{
+            print("Recording wasn't successful")
+        }
     }
     
 }
